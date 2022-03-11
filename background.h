1f34d77b0feccb404fb425571bf5c0add8db87a4
raniakhedri <rania.khedri@esprit.tn>
2022-03-11 10:12:23 +0100

    first add

---
+++ background.h
@@ -0,0 +1,26 @@
+#ifndef Image_H
+#define Image_H
+#include <SDL/SDL.h>
+#include <SDL/SDL_background.h>
+
+typedef struct
+{
+SDL_Surface *perso;
+SDL_Rect posperso;
+int direction;
+}Personne;
+typedef struct 
+{
+  SDL_Surface *imgBack;
+  SDL_Rect posBack;
+  SDL_Rect scroll;
+}Background;
+
+
+void initBack(Background *b);
+void afficherBack(Background b,SDL_Surface *ecran);
+void animerBackground( Background * e);
+int collisionPP( Personne p, SDL_Surface * Masque);
+void scrolling (background * b, int direction, int pasAvancement);
+void scrolling (SDL_Rect * b, int direction ,int pasAvancement);
+#endif
